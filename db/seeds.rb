b975d0704bfc6bd24865f08df48290de9d930621
Maria Vianello <mvian814@gmail.com>
2023-03-21 14:50:34 -0400

    handle 0 tasks, update seed dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,35 @@
 Task.create(
   title: "Iggy",
   note: "Seed the lettuce",
-  due_date: DateTime.new(2023, 11, 14, 9, 15, 0),
+  due_date: DateTime.new(2023, 1, 14, 9, 15, 0),
   effort_level: 2
 )
 
 Task.create(
   title: "Rosie",
   note: "Water the dog",
-  due_date: DateTime.new(2023, 11, 14, 9, 15, 0),
+  due_date: DateTime.new(2023, 2, 14, 9, 15, 0),
   effort_level: 1
 )
 
 Task.create(
   title: "Why I'm busy",
   note: "Tie my shoes",
-  due_date: DateTime.new(2023, 11, 14, 9, 15, 0),
+  due_date: DateTime.new(2023, 3, 14, 9, 15, 0),
   effort_level: 1
 )
 
 Task.create(
   title: "Gift for myself",
   note: "3D print a hanging monkey",
-  due_date: DateTime.new(2023, 11, 14, 9, 15, 0),
+  due_date: DateTime.new(2023, 4, 14, 9, 15, 0),
   effort_level: 2
 )
 
 Task.create(
   title: "I forgot to do something",
   note: "make a todo list crud app",
-  due_date: DateTime.new(2023, 11, 14, 9, 15, 0),
+  due_date: DateTime.new(2023, 5, 14, 9, 15, 0),
   effort_level: 3
 )
 

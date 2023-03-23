e085fb57ac3b6a132f8de08d009f41c6420e4436
Maria Vianello <mvian814@gmail.com>
2023-03-23 14:53:29 -0400

    created form partials

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,16 @@ module TasksHelper
   def border_bottom(tasks, task)
     return "border-bottom" if task != tasks.last
   end
+
+  def handle_show(disable_show)
+    disable_show ? "disable" : "green"
+  end
+
+  def handle_edit(disable_edit)
+    disable_edit ? "disable" : "blue"
+  end
+
+  def handle_completed(task_in_progress)
+    task_in_progress ? "disable" : "pink"
+  end
 end

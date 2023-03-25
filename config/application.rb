cee605a77994def0975df076cd83bd5f691405d0
Maria Vianello <mvian814@gmail.com>
2023-03-25 13:50:46 -0400

    update config to serve static assets

--- config/application.rb
+++ config/application.rb
@@ -43,5 +43,8 @@ module Todolist
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # serving assets for heroku
+    config.serve_static_assets = true
   end
 end
